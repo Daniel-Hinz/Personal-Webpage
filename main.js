dc08767e1f6951cb90b1529fa0af73ff497caa03
Daniel Hinz <dhinz1@kent.edu>
2021-05-11 16:30:05 -0400

    somewhat fixed resize issue

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 2000
 });
+
+function appHeight() {
+    const doc = document.documentElement
+    doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+  }
+
+  window.addEventListener('resize', appHeight);
+  appHeight();
