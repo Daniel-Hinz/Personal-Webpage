af18e9f7c21abed6e78d1c50ac732dbbe73c85ad
Daniel Hinz <dhinz1@kent.edu>
2021-05-13 14:53:26 -0400

    made fully responsive

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 2000
 });
 
-function appHeight() {
-    const doc = document.documentElement
-    doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
-  }
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-  window.addEventListener('resize', appHeight);
-  appHeight();
+
+window.addEventListener('resize', () => {
+	window.resizeTo(window.innerWidth, vh * 100);
+});
