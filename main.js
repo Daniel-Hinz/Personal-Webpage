be504cb32d032c6985c7ec3559c6817360042ad5
Daniel Hinz <dhinz1@kent.edu>
2021-06-04 09:47:36 -0400

    updated skills table

--- main.js
+++ main.js
@@ -11,6 +11,56 @@ window.addEventListener('resize', () => {
 	window.resizeTo(window.innerWidth, vh * 100);
 });
 
+// for skills table
+$(document).ready(function() {
+  $(window).on('load resize', function() {
+    var $thisnav = $('.active').offset().left;
+
+    $('.tab-header').hover(function() {
+      var $left = $(this).offset().left - $thisnav;
+      var $width = $(this).outerWidth();
+      $('.wee').css({ 'left': $left, 'width': $width * 0.5});
+    }, function() {
+      var $initwidth = $('.active').width();
+      $('.wee').css({ 'left': $left, 'width': $initwidth });
+    });
+  });
+});
+
+$(".languages-tab").click(function(){
+	$(".languages").css('display', 'block');
+	$(".software").css('display', 'none');
+	$(".version-control").css('display', 'none');
+
+	$(".languages-tab").addClass('active');
+	$(".software-tab").removeClass('active');
+	$(".version-tab").removeClass('active');
+});
+
+$(".software-tab").click(function(){
+	$(".languages").css('display', 'none');
+	$(".software").css('display', 'block');
+	$(".version-control").css('display', 'none');
+
+	$(".languages-tab").removeClass('active');
+	$(".software-tab").addClass('active');
+	$(".version-tab").removeClass('active');
+});
+
+$(".version-tab").click(function(){
+	$(".languages").css('display', 'none');
+	$(".software").css('display', 'none');
+	$(".version-control").css('display', 'block');
+
+	$(".languages-tab").removeClass('active');
+	$(".software-tab").removeClass('active');
+	$(".version-tab").addClass('active');
+});
+
+// exit button on projects display
+$(".fa-times").click(function(){
+	$(".project-display").css('display', 'none');});
+
 // project management
 $(".fa-times").click(function(){
 	$(".project-display").css('display', 'none');});
