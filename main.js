4d84921a2ff8466740d39cf6b293acaea3fd881b
Daniel Hinz <dhinz1@kent.edu>
2021-07-11 11:21:02 -0400

    fixed mobile browser issue

--- main.js
+++ main.js
@@ -3,14 +3,6 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000
 });
 
-// prevent vertical mobile browser resize
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-	window.resizeTo(window.innerHeight, vh * 100);
-});
-
 // for skills table
 $(document).ready(function() {
   var $thisnav = $('.active').offset().left;
