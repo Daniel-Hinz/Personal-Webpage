fcf7518f6a5988979392d12b5b282d33be93d34d
Daniel Hinz <dhinz1@kent.edu>
2022-04-27 13:47:16 -0400

    refactored javascript

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+let projectHeaders = document.querySelectorAll(".project-headers-header");
+let projectDisplays = document.querySelectorAll(".project-display");
+let exitButtons = document.querySelectorAll(".fa-times");
+let allProjects = document.querySelectorAll(".project");
+let headers = document.querySelectorAll(".tab-header");
+let tabs = document.querySelectorAll(".tab");
+let element = 0;
+
 // smooth scroll
 var scroll = new SmoothScroll('a[href*="#"]', {
   speed: 500,
@@ -42,190 +50,51 @@ $(document).ready(function () {
   });
 });
 
-// for skills table
-$(".languages-tab").click(function () {
-  $(".languages").css("display", "block");
-  $(".software").css("display", "none");
-  $(".version-control").css("display", "none");
+// change headers on skill table
+headers.forEach((header, index) => {
+  header.addEventListener("click", () => {
+    tabs.forEach((tab, i) => {
+      if (i === index) tab.style.display = "block";
+      else tab.style.display = "none";
+    });
+  });
 });
 
-$(".software-tab").click(function () {
-  $(".languages").css("display", "none");
-  $(".software").css("display", "block");
-  $(".version-control").css("display", "none");
+// change project headers
+projectHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    projectHeaders.forEach((item) => item.classList.remove("current"));
+    header.classList.add("current");
+
+    allProjects.forEach((project) => {
+      if ([...project.classList].includes(header.id))
+        project.style.display = "block";
+      else project.style.display = "none";
+    });
+  });
 });
 
-$(".version-tab").click(function () {
-  $(".languages").css("display", "none");
-  $(".software").css("display", "none");
-  $(".version-control").css("display", "block");
+// open project display
+allProjects.forEach((project, index) => {
+  project.addEventListener("click", () => {
+    projectDisplays[index].setAttribute("open", "");
+    projectDisplays[index].style.display = "block";
+    element = index;
+  });
 });
 
-// project headers
-$("#all-projects").click(function () {
-  $(".project-headers-header").removeClass("current");
-  $(this).addClass("current");
-  $(".project").css("display", "block");
-});
-
-$("#front-end").click(function () {
-  $(".project-headers-header").removeClass("current");
-  $(this).addClass("current");
-  $(".project").css("display", "none");
-  $(".front-end").css("display", "block");
-});
-
-$("#full-stack").click(function () {
-  $(".project-headers-header").removeClass("current");
-  $(this).addClass("current");
-  $(".project").css("display", "none");
-  $(".full-stack").css("display", "block");
-});
-
-// exit button on projects display
-let element = 0;
-$(".fa-times").click(function () {
-  switch (element) {
-    case 0:
-      const modal = document.querySelector(".project-zero-display");
-      modal.removeAttribute("open");
-      modal.setAttribute("closing", "");
-      modal.addEventListener(
-        "animationend",
-        () => {
-          modal.removeAttribute("closing");
-          modal.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-    case 1:
-      const modalOne = document.querySelector(".project-one-display");
-      modalOne.removeAttribute("open");
-      modalOne.setAttribute("closing", "");
-      modalOne.addEventListener(
-        "animationend",
-        () => {
-          modalOne.removeAttribute("closing");
-          modalOne.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-    case 2:
-      const modalTwo = document.querySelector(".project-two-display");
-      modalTwo.removeAttribute("open");
-      modalTwo.setAttribute("closing", "");
-      modalTwo.addEventListener(
-        "animationend",
-        () => {
-          modalTwo.removeAttribute("closing");
-          modalTwo.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-    case 3:
-      const modalThree = document.querySelector(".project-three-display");
-      modalThree.removeAttribute("open");
-      modalThree.setAttribute("closing", "");
-      modalThree.addEventListener(
-        "animationend",
-        () => {
-          modalThree.removeAttribute("closing");
-          modalThree.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-    case 4:
-      const modalFour = document.querySelector(".project-four-display");
-      modalFour.removeAttribute("open");
-      modalFour.setAttribute("closing", "");
-      modalFour.addEventListener(
-        "animationend",
-        () => {
-          modalFour.removeAttribute("closing");
-          modalFour.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-    case 5:
-      const modalFive = document.querySelector(".project-five-display");
-      modalFive.removeAttribute("open");
-      modalFive.setAttribute("closing", "");
-      modalFive.addEventListener(
-        "animationend",
-        () => {
-          modalFive.removeAttribute("closing");
-          modalFive.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-    case 6:
-      const modalSix = document.querySelector(".project-six-display");
-      modalSix.removeAttribute("open");
-      modalSix.setAttribute("closing", "");
-      modalSix.addEventListener(
-        "animationend",
-        () => {
-          modalSix.removeAttribute("closing");
-          modalSix.style.display = "none";
-        },
-        { once: true }
-      );
-      break;
-  }
-});
-
-// open and close projects management
-$(".zero").click(function () {
-  const modal = document.querySelector(".project-zero-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 0;
-});
-
-$(".one").click(function () {
-  const modal = document.querySelector(".project-one-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 1;
-});
-
-$(".two").click(function () {
-  const modal = document.querySelector(".project-two-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 2;
-});
-
-$(".three").click(function () {
-  const modal = document.querySelector(".project-three-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 3;
-});
-
-$(".four").click(function () {
-  const modal = document.querySelector(".project-four-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 4;
-});
-
-$(".five").click(function () {
-  const modal = document.querySelector(".project-five-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 5;
-});
-
-$(".six").click(function () {
-  const modal = document.querySelector(".project-six-display");
-  modal.setAttribute("open", "");
-  modal.style.display = "block";
-  element = 6;
+// close project display
+exitButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    projectDisplays[element].removeAttribute("open");
+    projectDisplays[element].setAttribute("closing", "");
+    projectDisplays[element].addEventListener(
+      "animationend",
+      () => {
+        projectDisplays[element].removeAttribute("closing");
+        projectDisplays[element].style.display = "none";
+      },
+      { once: true }
+    );
+  });
 });
